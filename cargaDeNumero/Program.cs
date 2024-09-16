338a3b6826fb70abaa29a0943c966ac1cc18fe27
Delvin Villar Arroyo <delvinvillararroyo280398@gmail.com>
2024-09-15 18:06:17 -0600

    Agregar archivos de proyecto.

---
+++ cargaDeNumero/Program.cs
@@ -0,0 +1,7 @@
+﻿using cargaDeNumero;
+
+punto punto = new punto();
+punto.cargarPunto(3, -4);
+
+punto.imprimirCuadrante();
+
---
+++ cargaDeNumero/punto.cs
@@ -0,0 +1,56 @@
+﻿using cargaDeNumero;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cargaDeNumero
+{
+    public class punto
+    {
+        private int x;
+        private int y;
+
+        public void cargarPunto(int x, int y)
+        {
+         this .x = x;
+         this .y = y;
+        }
+        public void imprimirCuadrante()
+        {
+            if (x > 0 && y > 0)
+            {
+                Console.WriteLine("el punto se encuentra en el primer cuadrante");
+            }
+            else if (x < 0 && y > 0)
+            {
+                Console.WriteLine("el punto se encuentra en el segundo cuadrante");
+            }
+            else if (x < 0 && y < 0)
+            {
+                Console.WriteLine("el punto se encuentra en el tercer cuadrante");
+            }
+            else if (x > 0 && y < 0)
+            {
+                Console.WriteLine("el punto se encuentra en elcuarto cuadrante");
+            }
+            else if (x == 0 && y != 0)
+            {
+                Console.WriteLine("el punto se encuentra en el eje y");
+            }
+            else if (x != 0 && y == 0)
+            {
+                Console.WriteLine("el punto se encuentra en el eje x");
+            }
+            else
+            {
+                Console.WriteLine("el punto se encuenta en el origen");
+            }
+
+
+
+
+        }
+    }
+}
